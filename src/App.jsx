50910d416d696ea87dfd4b61641df1542a600214
wildcard329 <enkiduwildman@gmail.com>
2023-10-18 20:35:39 -0700

    [pro-002] added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import PROJECTS from "./assets/configs/projects.json";
+import Header from './components/Header';
 import ProjectList from './components/ProjectList';
 
 const App = () => 
-  <div className="container">
-    <ProjectList projects={PROJECTS} />
+  <div>
+    <Header />
+    <section id="about">
+
+    </section>
+    <section id="projects">
+      <ProjectList projects={PROJECTS} />
+    </section>
+    <section id="contact">
+      
+    </section>
   </div>
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => 
+  <header>
+    <nav>
+      <div class="container">
+        <h1>Adam David</h1>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+export default Header;
